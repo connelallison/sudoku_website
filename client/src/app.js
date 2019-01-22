b8f08999b893cf7f1232742ab27216625178b514
Connel Allison <connelallison@hotmail.com>
2019-01-22 10:48:52 +0000

    basic stopwatch functionality is now implemented. residiual mess from merge should now be cleaned up

--- client/src/app.js
+++ client/src/app.js
@@ -1,67 +1,37 @@
 const Hub = require('./models/hub.js');
-<<<<<<< HEAD
 const Sudoku = require('./models/sudoku_solver.js');
 const SudokuValuesView = require("./views/sudoku_values_view.js");
 const SudokuCandidatesView = require("./views/sudoku_candidates_view.js");
 const PubSub = require('./helpers/pub_sub.js');
-
-=======
 const UserFormView = require('./views/user_form_view.js');
 const UserGridView = require('./views/user_grid_view.js');
 const Users = require('./models/users_model.js');
 const HistoryView = require('./views/history_view.js');
 const RulesView = require('./views/rules_view.js');
-const Sudoku = require('./models/sudoku_solver.js')
-const SudokuValuesView = require("./views/sudoku_values_view.js")
-const SudokuCandidatesView = require("./views/sudoku_candidates_view.js")
->>>>>>> a8e554d874cc73d64607cab494c8159b625a0c5f
+const Stopwatch = require("./views/stopwatch.js");
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('Javascript loaded');
 
-<<<<<<< HEAD
-// temporary code for passing a hardcoded sudoku to the view - will be replaced later.
-  const sudoku = new Sudoku();
-  const sudokuRows = [
-    [0, 0, 2, 9, 8, 0, 5, 0, 0],
-    [4, 0, 0, 0, 7, 0, 0, 1, 3],
-    [0, 3, 9, 6, 0, 4, 0, 7, 0],
-    [2, 0, 0, 0, 5, 6, 4, 0, 0],
-    [8, 4, 0, 3, 0, 0, 2, 0, 1],
-    [9, 0, 7, 0, 0, 1, 0, 8, 6],
-    [6, 0, 0, 7, 0, 5, 1, 3, 0],
-    [0, 9, 1, 4, 0, 0, 0, 0, 5],
-    [0, 2, 0, 0, 3, 0, 6, 0, 8]
-  ];
-  sudoku.populate2dArray(sudokuRows);
-  let sudokuValues = sudoku.unitsNumbers(sudoku.rows);
-  let sudokuCandidates = sudoku.unitsCandidates(sudoku.rows);
-
-  const switchViewButton = document.querySelector("#switch-view-button");
-  switchViewButton.addEventListener("click", () => {
-    PubSub.publish("App:switch-view-clicked", )
-  })
-
-  const sudokuGridDiv = document.querySelector("#sudoku-grid-div")
-  const sudokuValuesView = new SudokuValuesView(sudokuGridDiv);
-  sudokuValuesView.bindEvents();
-  // sudokuValuesView.render(sudokuValues);
-  const sudokuCandidatesView = new SudokuCandidatesView(sudokuGridDiv);
-  sudokuCandidatesView.bindEvents();
-
-  // sudokuCandidatesView.render(sudokuCandidates);
-
-
-  const hub = new Hub();
-  hub.bindEvents();
-  // hub.getDataEasy();
-=======
-  const rulesInfoContainer = document.querySelector('div#wrapper');
-  const rulesView = new RulesView(rulesInfoContainer);
-  rulesView.showRules();
-
-  const historyInfoContainer = document.querySelector('div#wrapper');
+  // // temporary code for passing a hardcoded sudoku to the view - will be replaced later.
+  //   const sudoku = new Sudoku();
+  //   const sudokuRows = [
+  //     [0, 0, 2, 9, 8, 0, 5, 0, 0],
+  //     [4, 0, 0, 0, 7, 0, 0, 1, 3],
+  //     [0, 3, 9, 6, 0, 4, 0, 7, 0],
+  //     [2, 0, 0, 0, 5, 6, 4, 0, 0],
+  //     [8, 4, 0, 3, 0, 0, 2, 0, 1],
+  //     [9, 0, 7, 0, 0, 1, 0, 8, 6],
+  //     [6, 0, 0, 7, 0, 5, 1, 3, 0],
+  //     [0, 9, 1, 4, 0, 0, 0, 0, 5],
+  //     [0, 2, 0, 0, 3, 0, 6, 0, 8]
+  //   ];
+  // sudoku.populate2dArray(sudokuRows);
+  // let sudokuValues = sudoku.unitsNumbers(sudoku.rows);
+  // let sudokuCandidates = sudoku.unitsCandidates(sudoku.rows);  const historyInfoContainer = document.querySelector('div#wrapper');
+  const historyInfoContainer = document.querySelector("div#wrapper")
   const historyView = new HistoryView(historyInfoContainer);
   historyView.showHistory();
 
@@ -78,34 +48,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const users = new Users(usersUrl);
   users.getData();
 
-  // temporary code for passing a hardcoded sudoku to the view - will be replaced later.
-    const sudoku = new Sudoku();
-    const sudokuRows = [
-      [0, 0, 2, 9, 8, 0, 5, 0, 0],
-      [4, 0, 0, 0, 7, 0, 0, 1, 3],
-      [0, 3, 9, 6, 0, 4, 0, 7, 0],
-      [2, 0, 0, 0, 5, 6, 4, 0, 0],
-      [8, 4, 0, 3, 0, 0, 2, 0, 1],
-      [9, 0, 7, 0, 0, 1, 0, 8, 6],
-      [6, 0, 0, 7, 0, 5, 1, 3, 0],
-      [0, 9, 1, 4, 0, 0, 0, 0, 5],
-      [0, 2, 0, 0, 3, 0, 6, 0, 8]
-    ];
-    sudoku.populate2dArray(sudokuRows);
-    let sudokuValues = sudoku.unitsNumbers(sudoku.rows);
-    let sudokuCandidates = sudoku.unitsCandidates(sudoku.rows);
+  const switchViewButton = document.querySelector("#switch-view-button");
+  switchViewButton.addEventListener("click", () => {
+    if (switchViewButton.innerHTML === "Show Candidates") {
+      switchViewButton.innerHTML === "Show Values"
+    }
+    if (switchViewButton.innerHTML === "Show Values") {
+      switchViewButton.innerHTML = "Show Candidates"
+    }
+    PubSub.publish("App:switch-view-clicked", )
+  })
 
-    const sudokuGridDiv = document.querySelector("#sudoku-grid-div")
-    const sudokuValuesView = new SudokuValuesView(sudokuGridDiv);
-    sudokuValuesView.render(sudokuValues);
-    // const sudokuCandidatesView = new SudokuCandidatesView(sudokuGridDiv);
-    // sudokuCandidatesView.render(sudokuCandidates);
+  const sudokuGridDiv = document.querySelector("#sudoku-grid-div")
+  const sudokuValuesView = new SudokuValuesView(sudokuGridDiv);
+  sudokuValuesView.bindEvents();
+  // sudokuValuesView.render(sudokuValues);
+  const sudokuCandidatesView = new SudokuCandidatesView(sudokuGridDiv);
+  sudokuCandidatesView.bindEvents();
 
-  const stopwatch = new Stopwatch();
+  const stopwatchContainer = document.querySelector("#stopwatch-container");
+  const stopwatch = new Stopwatch(stopwatchContainer);
   stopwatch.bindEvents();
+  // sudokuCandidatesView.render(sudokuCandidates);
+
 
   const hub = new Hub();
-    // hub.bindEvents();
-  hub.getData();
->>>>>>> a8e554d874cc73d64607cab494c8159b625a0c5f
+  hub.bindEvents();
 })
--- client/src/views/stopwatch.js
+++ client/src/views/stopwatch.js
@@ -1,11 +1,76 @@
+const PubSub = require('../helpers/pub_sub.js');
 
 const Stopwatch = function(container){
   this.container = container
+  this.status = 0;
+  this.time = 0;
 }
 
+Stopwatch.prototype.populate = function () {
+  // const title = document.createElement("h3");
+  const timerLabel = document.createElement("h2");
+  // title.textContent = "Stopwatch";
+  timerLabel.id = "timer-label"
+  // this.container.appendChild(title);
+  this.container.appendChild(timerLabel);
+  this.status = 1;
+  this.timer();
+}
+
+
+
+
+Stopwatch.prototype.timer = function () {
+  const stopwatch = this;
+  if (stopwatch.status === 1) {
+    setTimeout(function (){
+      // console.log(stopwatch);
+      // console.log(stopwatch.time);
+      stopwatch.time++;
+      let hour = Math.floor(stopwatch.time/60/60);
+      let min = Math.floor(stopwatch.time/60);
+      let sec = Math.floor(stopwatch.time);
+
+      if (hour < 10) {
+        hour = "0" + hour + ":";
+      }
+      if (hour === 0) {
+        hour = "";
+      }
+      if (min >= 60) {
+        min = min % 60 + ":";
+      }
+      if (min < 10) {
+        min = "0" + min + ":";
+      }
+      if (min === 0) {
+        min = "";
+      }
+      if (sec >= 60) {
+        sec = sec % 60;
+      }
+      if (sec < 10) {
+        sec = "0" + sec;
+      }
+
+      document.querySelector("#timer-label").innerHTML = `${hour}${min}${sec}`
+      // console.log(hour + min + sec);
+      stopwatch.timer();
+    }, 1000);
+  }
+}
+
+
 Stopwatch.prototype.bindEvents = function(){
-  const stopwatch = document.querySelector("stopwatchContainer");
-  stopwatch.addEventListener('click', (event) => {
-    console.log('click', event.detail)
+  // stopwatch.addEventListener('click', (event) => {
+  //   console.log('click', event.detail)
+  // })
+  PubSub.subscribe("Hub:puzzle-begins", () => {
+    this.populate();
+  })
+  PubSub.subscribe("Hub:puzzle-ends", () => {
+    this.status = 0;
   })
 }
+
+module.exports = Stopwatch;
